e371adeee27c757e08ec1c8e88935b52a53bf74d
Wilson Santos <38737107+wilcat@users.noreply.github.com>
2024-11-13 12:04:49 -0300

    Add files via upload

--- templates/header.php
+++ templates/header.php
@@ -3,11 +3,15 @@
 ?>
 <header>
     <h1>Sistema de Controle de Estoque</h1>
+        <div style="display: block; margin:auto" class=img>
+            <img src=" ../img/FALCÃO_NET.png" width=20% >
+        </div> 
     <nav>
         <a href="index.php">Início</a> |
         <a href="produtos.php">Produtos</a> |
         <a href="usuarios.php">Usuários</a> |
         <a href="relatorios.php">Relatórios</a> |
+        <a href="ordem_servico.php">Ordem de Serviços</a> |
         <a href="logout.php">Logout</a>
     </nav>
     <hr>
